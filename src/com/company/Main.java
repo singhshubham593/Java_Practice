ccafc3f12baa61d7d042f33dbb379d9493dbec01
Shubham Singh <rajputshubhamsingh593@gmail.com>
2022-01-12 19:18:42 +0530

    added college name

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code her
         System.out.println("My name is shubham singh");
+        System.out.println("My college name is MCKV");
     }
 }
