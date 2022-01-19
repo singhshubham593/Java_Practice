3843f9edd06ed0ae264ab19476f8701be158a546
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-01-19 15:23:13 +0530

    conditional commit

---
+++ condition.java
@@ -0,0 +1,34 @@
+public class condition {
+    public static void main(String[] args) {
+    /*
+      Syntex of if ststements:
+      if(boolean expression t or f){
+          // body
+          } esle{
+          // do this
+          }
+     */
+        /* example
+        int salery = 2004;
+        if (salery > 2000){
+            salery = salery + 100;
+        } else {
+            salery = salery + 200;
+        }
+        System.out.println(salery);
+
+         */
+        /* Multiple if esle statement
+         int salery =254;
+         if( salery >100){
+          salery +=10;
+          } else if(salery >200){
+           salery +=20;
+          } else{
+          salery += 30;
+          }
+          System.out.println(salery);
+         */
+
+    }
+}
