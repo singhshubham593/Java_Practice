35e9c3e93f5b4299f0ed2cfb867e688bbb8cc4c8
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-01-20 07:17:46 +0530

    initial commit

---
+++ loops.java
@@ -0,0 +1,33 @@
+public class loops {
+    public static void main(String[] args) {
+        /*
+      //  Syntax of for loop ;
+
+         for( initialisation; condition; increment/decrement) {
+                 // body
+                 }
+
+
+        //Example = print number from 1 to 5;
+
+        for(int num=1; num<=5; num+=1){
+            System.out.println(num);
+        }
+
+           // while loop Syntex;
+
+           while(condition){
+               // body
+           }
+
+
+            // Example = print number from 1 to 5;
+
+            int num =1;
+            while (num<=5){
+                System.out.println(num);
+                num ++;
+            }
+            */
+    }
+}
