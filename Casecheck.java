c3abb1998754ca4ab6dbb6b900e0abda90568c0f
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-01-20 11:11:47 +0530

    casechack commit

---
+++ Casecheck.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Casecheck {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        // case chack of alphabet;
+
+        char ch =in.next().trim().charAt(2); // 2 = can be varaible with
+         if(ch >= 'a' && ch <='z'){
+             System.out.println("Lowercase");
+         }
+          else{
+              System.out.println("Uppercase");
+         }
+          System.out.println(ch);
+
+    }
+}
