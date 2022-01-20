21224e2eb32ed2ea698d2ab495ae6cb1da47e55c
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-01-20 09:39:42 +0530

    find largest no commit

---
+++ largestno.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class largestno {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        // find the largest in 3 no;
+
+        int a =in.nextInt();
+        int b =in.nextInt();
+        int c =in.nextInt();
+
+         int max = a;
+
+         if (b>max){
+             max =b;
+         }
+        if (c > max) {
+            max=c;
+        }
+         System.out.println(max);
+    }
+}
