aff983a65870eff26f989293465a82d2cac89adc
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-01-22 08:36:31 +0530

    Calculator program commit

---
+++ Calculator.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+
+        Scanner in =new Scanner(System.in);
+         // take input from user till user does not press X or x;
+        int ans = 0;
+        while (true) {
+           // take the operator as input;
+            System.out.print("Enter the operator: ");
+           char op =in.next().trim().charAt(0);
+
+           if(op =='+' || op =='-' || op =='*' || op =='/' || op =='%'){
+               //Input two number;
+               System.out.print("Enter two number: ");
+               int num1 = in.nextInt();
+               int num2 = in.nextInt();
+
+               if(op == '+'){
+                   ans = num1 +num2;
+               }
+                if(op == '-'){
+                    ans = num1 -num2;
+                }
+                 if(op == '*'){
+                     ans =num1 *num2;
+                 }
+                  if(op == '/'){
+                    if(num2 !=0) {
+                      ans = num1 /num2;
+                    }
+                  }
+                   if(op == '%'){
+                       ans = num1 %num2;
+                   }
+           }
+           else if(op =='x' || op =='X'){
+               break;
+           }
+           else {
+               System.out.println("Invalid operation");
+           }
+           System.out.println(ans);
+        }
+    }
+}
