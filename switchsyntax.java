ce4b81a03b10c4c0ba234f0346ba1acea486d1a0
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-01-23 15:26:06 +0530

    switch syntex commit

---
+++ switchsyntax.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class switchsyntax {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int X =in.nextInt();
+        int Y =in.nextInt();
+
+        // Syntax;
+
+        switch (exppression){
+            case X:              //X = one;
+                //do something;
+            case Y:              //Y = two;
+                //do something;
+            default:
+                //do something;
+        }
+    }
+}
