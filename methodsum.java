9e82ce6a9ef9374fe93828e797c7999b651ded6a
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-01-26 14:45:18 +0530

    method with example
    method with example

--- methodsum.java
+++ methodsum.java
@@ -2,25 +2,25 @@ import java.util.Scanner;
 
 public class methodsum {
     public static void main(String[] args) {
-       sum();
-       sum();
-       sum();   // we can use  some for many time if we want;
+        sum();
+        sum();   // we can use  some for many time if we want;
     }
-      // Syntex of method;
+    // Syntex of method;
         /*
         access modifier (we'll look in oop) return_tpye name () {
              // body ;
              return statement;
          */
-        static void sum() {  // At place sum, we can use( -,*,/ e.t.c)
-            Scanner in =new Scanner(System.in);
-            System.out.println("Enter num 1:");
-            int num1=in.nextInt();
-            System.out.println("Enter num 2:");
-            int num2=in.nextInt();
-            int sum =  num1 +  num2;
-            System.out.println("The sum of num1 and num2 is "+sum);
+    static void sum() {  // At place sum, we can use( -,*,/ e.t.c)
+        Scanner in =new Scanner(System.in);
+        System.out.println("Enter num 1:");
+        int num1=in.nextInt();
+        System.out.println("Enter num 2:");
+        int num2=in.nextInt();
+        int sum =  num1 +  num2;
+        System.out.println("The sum of num1 and num2 is "+sum);
 
-        }
+    }
 
 }
+
