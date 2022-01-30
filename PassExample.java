77988c50968c9596d0290ef3a884efd001707203
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-01-30 10:30:47 +0530

    pass value commit

---
+++ PassExample.java
@@ -0,0 +1,12 @@
+public class PassExample {
+    public static void main(String[] args) {
+        String name = "Shubham Singh";
+        greet(name);
+    }
+
+    static void greet(String naam) { // in this case name or naam contain same name"Shubham Singh";
+        System.out.println(naam); // in java, It is contain value olny,not refirsatio;
+                                  // if create anthor object,then it wiil take previous one;
+    }
+
+}
