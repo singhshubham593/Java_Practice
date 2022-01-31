d15931408644db2ee4d0ccd85738e4efc2bd4e3f
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-01-31 11:38:34 +0530

    variable ad argument

---
+++ VarArgs.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class VarArgs {
+    public static void main(String[] args) {
+        /*
+        //for single variable Argument;
+        fun(4,7,4,8,9);
+    }
+    static void fun(int...v){
+        System.out.println(Arrays.toString(v));
+
+         */
+
+        // for multiple variable Argument;
+        multiple(3, 4, "Shubham Singh", "Raghu", "Shyamu");
+
+    }
+
+    static void multiple(int a, int b, String... v) {
+        System.out.println(Arrays.toString(v));
+
+    }
+}
