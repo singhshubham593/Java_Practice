64de73a793ea61d70980060e9626279776a2266e
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-02-02 23:39:08 +0530

    Arrays syntex

---
+++ arraySyntex.java
@@ -0,0 +1,15 @@
+public class arraySyntex {
+    public static void main(String[] args) {
+      // Syntex
+
+      // datatype[] variable_name = new datatype[size];
+
+      // store 5 roll no;
+
+      int[] rnos = new int[5];
+
+      //or directly
+        int[] rnos2 = {23,43,75,87};
+        System.out.println(rnos2[1]);
+    }
+}
