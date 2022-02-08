9911d1b2cf56405077b6bc655e35a88e05b74ce7
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-02-08 19:35:25 +0530

    my intro

--- helloworld.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class helloworld {
-    public static void main(String[] args) {
-        System.out.println("hello world");
-
-    }
-}
---
+++ introduction.java
@@ -0,0 +1,13 @@
+public class introduction {
+    public static void main(String[] args) {
+        System.out.println("Name = Shubham Singh");
+        System.out.println("Collage = MCKV Institute Of Engineering");
+        System.out.println("Father Name = Manoj kumar singh");
+        System.out.println("Mother Name = asha devi");
+        System.out.println("Grandfather Name = Raj kumar singh");
+        System.out.print("Adddess = Mahdah,");
+        System.out.print("Buxar,");
+        System.out.print("Bihar.");
+
+    }
+}
