f221f8aa1e11fc3465b5205e991a5e85848f4a40
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-03-03 20:05:37 +0530

    casting

---
+++ casting.java
@@ -0,0 +1,12 @@
+public class casting {
+    public static void main(String[] args) {
+        // casting = means work for revercing processing;
+        double price = 100.00;
+        double finalprice = price + 18 ;
+        System.out.println(finalprice);
+        //
+        int p =100;
+        int fp = p +(int) 18.00;
+        System.out.println(fp);
+    }
+}
