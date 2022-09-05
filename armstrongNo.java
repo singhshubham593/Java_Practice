0953e14ef5cb6e8ff383fceebdb114473161bc26
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-09-05 22:48:11 +0530

    java : Armstrong number

--- armstrongNo.java
+++ armstrongNo.java
@@ -2,21 +2,26 @@ import java.util.Scanner;
 // Armstrong Number
 public class armstrongNo {
     public static void main(String[] args) {
-
+        // using scanner
         Scanner sc = new Scanner(System.in);
+        // taking intput
         int num = sc.nextInt();
+        // function calling and printing output
         System.out.println(armstro(num));
 
     }
+    //creating new function
     static boolean  armstro(int num){
         int ori=num;
         int sum =0;
+        // for armstrong
         while (num>0){
             int rem = num % 10;
             rem =rem /10;
             int remcub =rem * rem * rem;
             sum = sum + remcub;
         }
+        // checking condition
         if (sum == ori){
             return true;
         }
