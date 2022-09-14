1ba14bf08a11e8d2bc7eedca93ea99fcb8b9e931
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-09-14 22:09:44 +0530

    java:Changing the value of array at index.

--- ChangeValue.java
+++ ChangeValue.java
@@ -1,5 +1,5 @@
 import java.util.Arrays;
-
+// Change value of array at index no 0.
 public class ChangeValue {
     public static void main(String[] args) {
 
@@ -8,7 +8,6 @@ public class ChangeValue {
         System.out.println(Arrays.toString(arr));
     }
      static void change(int[] nums){
-
         nums[0]=77;//ijf you make a change to the object via this ref  variable,same object will be changed;
     }
 }
