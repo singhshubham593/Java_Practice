85c189be3b947bab145414079418653264bc4ccd
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-09-15 21:55:39 +0530

    java:how many times 6 is coming in 6473636;

--- Countnum.java
+++ Countnum.java
@@ -1,16 +1,16 @@
 public class Countnum {
     // find how many times 6 is coming in 6473636;
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
-        int n =6473636;
-        int count = 0;
-
-        while (n > 0) {
-            int rem =n %10;
-            if (rem == 6) {
+        int n = 6473636 ;
+        int count=0;
+        
+        while(n>0){
+            int rem = n%10 ;
+            if(rem == 6){
                 count ++;
             }
-            n =n/10; // n/= 10;
+            n=n/10; // n/= 10;
         }
         System.out.println(count);
     }
