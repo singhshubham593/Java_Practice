75908abb7892a4512fbf3fec29a7cef9bc5e3947
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-09-18 23:39:11 +0530

    java:Reverse Searching

--- reversearr.java
+++ reversearr.java
@@ -1,11 +1,12 @@
 import java.util.Arrays;
-
+// reverse searching
 public class reversearr {
     public static void main(String[] args) {
         int[] arr ={1,3,6,28,9};
          reverse(arr);
         System.out.println(Arrays.toString(arr));
     }
+    // new function for reverse
     static void reverse(int[] arr){
         int start =0;
         int end = arr.length-1;
@@ -16,6 +17,7 @@ public class reversearr {
             end--;
         }
     }
+    // new function for swapping
     static void swap(int[] arr,int index1,int index2){
         int temp;
         temp=arr[index1];
