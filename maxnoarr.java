3add9000b29f21fc3ea53ae51f7da003c0770fa7
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-09-25 21:42:11 +0530

    java : finding maximum number in array.

--- maxnoarr.java
+++ maxnoarr.java
@@ -1,8 +1,10 @@
+// finding maximum number in array.
 public class maxnoarr {
     public static void main(String[] args) {
         int[] arr ={1,3,6,28,9};
         System.out.println(max(arr));
     }
+     // Method for maximum number.
      static int max(int[] arr) {
         int maxVal = arr[0];
         for (int i = 0; i < arr.length; i++) {
