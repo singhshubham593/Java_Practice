fbdf76f1bf7057eb56d6978617f40cf6d67391f2
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-10-07 21:11:20 +0530

    java : String.

--- string.java
+++ string.java
@@ -10,5 +10,18 @@ public class string {
         for( int i=0; i<fullName.length(); i++){
             System.out.println(fullName.charAt(i)); // for index 0 to n ,to print all world
         }
+        // Comprare
+        String name1 = "Sonny";
+        String name2 = "sonny";
+
+        // case1 : s1>s2 : +ve value
+        //case2 : s==s2 :0
+        //case3 : s1<s2 :-ve value
+
+        if(name1.compareTo(name2)==0){
+            System.out.println("String are equal.");
+        }else{
+            System.out.println("String are not equal.");
+        }
     }
 }
