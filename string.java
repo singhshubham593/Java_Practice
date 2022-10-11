5ee69b9e69c8db874b66f1cdad8c96d0f1763453
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-10-11 20:11:57 +0530

    java : String.

--- string.java
+++ string.java
@@ -1,8 +1,9 @@
+// String
 public class string {
     public static void main(String[] args) {
         //concatenation
         String firstFame = "Shubham";
-        String lastName = " Singh";
+        String lastName = "Singh";
         String fullName = firstFame +" "+lastName; // addition of two string
         System.out.println(fullName);
 
